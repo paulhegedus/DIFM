7ba69ada00253660f60b8b31812bb0e4fa9f5f36
Taro Mieno <tmieno2@unl.edu>
2021-02-23 10:12:39 -0600

    files

--- tools_for_analsis_team.R
+++ tools_for_analsis_team.R
@@ -45,7 +45,7 @@ field_year_ls <- field_data[year == 2020, ]$field_year
 
 #--- individually ---#
 exp_process_make_report(
-  ffy = field_year_ls[5],
+  ffy = field_year_ls[6],
   rerun = TRUE
 )
 
