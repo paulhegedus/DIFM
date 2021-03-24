7d3e69567ff878111e6a5330e3e2b2ea2e5771fb
Taro Mieno <tmieno2@unl.edu>
2021-03-24 11:04:17 -0500

    a

--- tools_for_trial_design_team.R
+++ tools_for_trial_design_team.R
@@ -41,7 +41,7 @@ make_trial_design(
   ffy, 
   json_file = "fp_2021_TD.json", 
   num_levels = 5, 
-  head_dist = c(120), 
+  head_dist = c(120, 120), 
   side_plots_num = 1,
   use_ab = TRUE,
   assign_rates = FALSE,
