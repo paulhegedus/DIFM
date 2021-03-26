361127887ef7f991fb2b5391998fd9d20b08406e
Taro Mieno <tmieno2@unl.edu>
2021-03-26 10:26:38 -0500

    a

--- tools_for_trial_design_team.R
+++ tools_for_trial_design_team.R
@@ -31,7 +31,7 @@ field_year_ls <- field_data$field_year
 #' # Make trial designs
 #/*=================================================*/
 
-ffy <- field_year_ls[15]
+ffy <- field_year_ls[14]
 
 get_td_parameters(ffy, "fp_2021_TD.json")
 
