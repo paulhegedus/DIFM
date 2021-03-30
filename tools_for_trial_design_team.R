b359ee1c809705480a49ae57354ca7f0939c9e99
Taro Mieno <tmieno2@unl.edu>
2021-03-30 14:16:57 -0500

    a

--- tools_for_trial_design_team.R
+++ tools_for_trial_design_team.R
@@ -38,10 +38,10 @@ get_td_parameters(ffy, "fp_2021_TD.json")
 #--- force headland to be 100 feet ---#
 
 make_trial_design(
-  ffy = field_year_ls[16], 
+  ffy = field_year_ls[14], 
   json_file = "fp_2021_TD.json", 
   # head_dist = 120, # default uses 2 * the max of the input plot widths
-  # side_dist = 30,
+  # side_dist = 30, # default uses the max of the section width (minimum is 30)
   #=== angled harvesting ===#
   # harvest angle relative to the planter/applicator
   # TRUE: uses both harvester and planter/applicator ab-lines or paste data
