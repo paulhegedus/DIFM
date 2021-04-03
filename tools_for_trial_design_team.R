b4e9d9393e80e41a31a5abeac4e88bdc6f15cc02
Taro Mieno <tmieno2@unl.edu>
2021-04-03 10:17:53 -0500

    a

--- tools_for_trial_design_team.R
+++ tools_for_trial_design_team.R
@@ -37,47 +37,13 @@ ffy <- field_year_ls[17]
 get_td_parameters(ffy, "fp_2021_TD.json")
 
 #--- force headland to be 100 feet ---#
-
 make_trial_design(
-  ffy = field_year_ls[12], 
+  ffy = field_year_ls[20], 
   json_file = "fp_2021_TD.json", 
   # head_dist = 120, # default uses 2 * the max of the input plot widths
   # side_dist = 30, # default uses the max of the section width (minimum is 30)
   #=== angled harvesting ===#
   plot_heading = "ab-line-angled-h",
-  #=== design type ===#
-  # jcl: jump-conscious latin
-  # ejca: extra jump-conscious alternate strip
-  # design_type = c("ejca", "ejca"),
-  #=== user-specified rates ===#
-  # rates = list(c(40, 50, 60, 70, 80)),
-  #=== number of levels ===#
-  # used in "jcl", ignored in "ejca"
-  # num_levels = c(6, 6), 
-  #=== maximum jump allowed  ===#
-  # used in "ejca", ignored in "jcl"
-  # max_jumps = c(NA, NA),
-  #=== file name ===#
-  file_name_append = "angled"
-)
-
-
-
-
-#/*=================================================*/
-#' # DevTeam
-#/*=================================================*/
-ffy <- field_year_ls[28]
-
-get_td_parameters(ffy, "fp_2021_TD.json")
-
-make_trial_design(
-  ffy = field_year_ls[12], 
-  json_file = "fp_2021_TD.json", 
-  # head_dist = 120, # default uses 2 * the max of the input plot widths
-  # side_dist = 30, # default uses the max of the section width (minimum is 30)
-  #=== angled harvesting ===#
-  plot_heading = "ab-line",
   #=== design type ===#
   # jcl: jump-conscious latin
   # ejca: extra jump-conscious alternate strip
@@ -91,7 +57,37 @@ make_trial_design(
   # used in "ejca", ignored in "jcl"
   # max_jumps = c(NA, NA),
   #=== file name ===#
-  file_name_append = NA,
+  file_name_append = "angled"
+)
+
+#/*=================================================*/
+#' # DevTeam
+#/*=================================================*/
+ffy <- field_year_ls[28]
+
+get_td_parameters(ffy, "fp_2021_TD.json")
+
+make_trial_design(
+  ffy = field_year_ls[20], 
+  json_file = "fp_2021_TD.json", 
+  # head_dist = 120, # default uses 2 * the max of the input plot widths
+  # side_dist = 30, # default uses the max of the section width (minimum is 30)
+  #=== angled harvesting ===#
+  plot_heading = "ab-line-angled-h",
+  #=== design type ===#
+  # jcl: jump-conscious latin
+  # ejca: extra jump-conscious alternate strip
+  # design_type = "ejca",
+  #=== user-specified rates ===#
+  # rates = list(c(40, 50, 60, 70, 80)),
+  #=== number of levels ===#
+  # used in "jcl", ignored in "ejca"
+  # num_levels = c(6, 6), 
+  #=== maximum jump allowed  ===#
+  # used in "ejca", ignored in "jcl"
+  # max_jumps = c(NA, NA),
+  #=== file name ===#
+  file_name_append = "angled",
   locally_run = TRUE
 )
 
